b826c0dbbfc913d6aceb2a6e74663ef5c3321ed8
SanzinaHossain <sanzina67hossain@gmail.com>
2022-07-29 15:50:55 +0600

    database connected

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ app.use(cors())
 app.use(express.json())
 
 //database connect
-const uri = "mongodb+srv://school_admin:<password>@cluster0.obuisi6.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obuisi6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  console.log("connected")
 });
 
 app.get('/', (req, res) => {
