fbcf0afb81b512f77457d9179c2f8ce6c3712bf7
SanzinaHossain <sanzina67hossain@gmail.com>
2022-07-29 16:09:19 +0600

    gallery data

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ app.use(express.json())
 //database connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obuisi6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected")
-});
+
+async function run(){
+  try{
+         await client.connect();
+         const galleryCollection=client.db('gallery').collection('gallerydata');
+         app.get('/gallerydata',async (req,res)=>{
+            const query={};
+            const cursor=galleryCollection.find(query)
+            const gallery= await cursor.toArray()
+            res.send(gallery)
+         })
+         console.log("database connected")
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
+  
 
 app.get('/', (req, res) => {
   res.send('School Website')
